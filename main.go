91861af9ab7b4553566877cb71505427ef70d803
fluteds <fluteds@users.noreply.github.com>
2025-03-16 00:19:57 +0000

    add multiple boards parsing with  -b

--- main.go
+++ main.go
@@ -17,15 +17,15 @@ import (
 var URLRegex = xurls.Strict()
 
 var options struct {
-	boardName string
-	pages     uint
-	replies   uint
+	boardNames string
+	pages      uint
+	replies    uint
 }
 
 func init() {
 	flag.UintVar(&options.replies, "n", 10, "cutoff of number of replies on thread")
 	flag.UintVar(&options.pages, "p", 1, "number of pages/request to get/make")
-	flag.StringVar(&options.boardName, "b", "news", "board name")
+	flag.StringVar(&options.boardNames, "b", "news", "comma-separated list of board names")
 }
 
 func main() {
@@ -45,23 +45,25 @@ func run() (string, error) {
 	flag.Parse()
 	now := time.Now()
 	feed := &feeds.Feed{
-		Title: fmt.Sprintf("4chan /%s/ threads", options.boardName),
-		Link: &feeds.Link{
-			Href: fmt.Sprintf("https://boards.4channel.org/%s/", options.boardName),
-		},
-		Description: fmt.Sprintf(
-			"threads from /%s/ with more than %d comments",
-			options.boardName,
-			options.replies,
-		),
-		Author:  &feeds.Author{Name: "Anon"},
-		Created: now,
+		Title:       fmt.Sprintf("4chan threads from multiple boards"),
+		Link:        &feeds.Link{Href: "https://boards.4channel.org/"},
+		Description: fmt.Sprintf("threads from multiple boards with more than %d comments", options.replies),
+		Author:      &feeds.Author{Name: "Anon"},
+		Created:     now,
 	}
-	threads, err := getThreads(options.boardName, options.pages)
-	if err != nil {
-		return "", err
+
+	boards := strings.Split(options.boardNames, ",")
+	var allItems []*feeds.Item
+	for _, board := range boards {
+		threads, err := getThreads(board, options.pages)
+		if err != nil {
+			return "", err
+		}
+		items := processThreads(threads, board)
+		allItems = append(allItems, items...)
 	}
-	feed.Items = processThreads(threads)
+
+	feed.Items = allItems
 	atom, err := feed.ToAtom()
 	if err != nil {
 		return "", err
@@ -80,24 +82,24 @@ func getThreads(board string, pages uint) (threads []*api.Thread, err error) {
 	return
 }
 
-func processThreads(threads []*api.Thread) []*feeds.Item {
+func processThreads(threads []*api.Thread, board string) []*feeds.Item {
 	var items []*feeds.Item
 	for _, thread := range threads {
 		if thread.Replies() < int(options.replies) {
 			continue
 		}
-		item := processPost(thread.OP)
+		item := processPost(thread.OP, board)
 		item.Title = fmt.Sprintf("[%3d] %s", min(999, thread.Replies()), item.Title)
 		items = append(items, item)
 	}
 	return items
 }
 
-func processPost(post *api.Post) *feeds.Item {
+func processPost(post *api.Post, board string) *feeds.Item {
 	item := &feeds.Item{}
 	item.Title = getTitle(post)
 	item.Link = &feeds.Link{
-		Href: fmt.Sprintf("https://boards.4channel.org/%s/thread/%d/", options.boardName, post.Id),
+		Href: fmt.Sprintf("https://boards.4channel.org/%s/thread/%d/", board, post.Id),
 	}
 	item.Description = anchorize(strings.ReplaceAll(post.Comment, "<wbr>", ""))
 	if post.File != nil {
